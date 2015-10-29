27cf7541e4bdb6efa724306895871948870840b8
Dave May <dave@davemay.org>
2015-10-29 17:28:08 -0600

    Setup and Teardown, after refactoring spec with beforeEach().

--- 02/spec/InvestmentSpec.js
+++ 02/spec/InvestmentSpec.js
@@ -1,19 +1,21 @@
 describe("Investment", function() {
-    it("should be of a stock", function() {
-        var stock = new Stock();
-        var investment = new Investment({
+    var stock;
+    var investment;
+
+    beforeEach(function() {
+        stock = new Stock();
+        investment = new Investment({
             stock: stock,
             shares: 100
         });
-        expect(investment.stock).toBe(stock);
+    });
+
+    it("should be of a stock", function() {
+       expect(investment.stock).toBe(stock);
     });
 
     it("should have the invested shares quantity", function() {
-        var stock = new Stock();
-        var investment = new Investment({
-            stock: stock,
-            shares: 100
-        });
         expect(investment.shares).toEqual(100);
     });
+
 });
