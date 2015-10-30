3c3684192da95b0426684927bbb0cf18d38bf112
Dave May <dave@davemay.org>
2015-10-30 10:44:34 -0600

    Coding a spec with shared behavior: Created a nested describe with QTY#2 new, non-existent functions- investment.roi() and investment.isGood(). Need to add these missing functions to Investment.prototype.

--- 02/spec/InvestmentSpec.js
+++ 02/spec/InvestmentSpec.js
@@ -1,10 +1,10 @@
-describe("Investment", function() {
+describe( "Investment", function() {
     var stock;
     var investment;
 
-    beforeEach(function() {
+    beforeEach( function() {
         stock = new Stock();
-        investment = new Investment({
+        investment = new Investment( {
             stock: stock,
             shares: 100,
             sharePrice: 20
@@ -12,24 +12,32 @@ describe("Investment", function() {
     });
 
     it("should be of a stock", function() {
-       expect(investment.stock).toBe(stock);
+       expect( investment.stock ).toBe( stock );
     });
 
     it("should have the invested shares quantity", function() {
-        expect(investment.shares).toEqual(100);
+        expect( investment.shares ).toEqual( 100 );
     });
 
     it("should have the share payed price", function() {
-       expect(investment.sharePrice).toEqual(20);
+       expect(investment.sharePrice).toEqual( 20 );
     });
 
-    it("should have a cost", function(){
-        expect(investment.cost).toEqual(2000);
+    it("should have a cost", function() {
+        expect( investment.cost ).toEqual( 2000 );
     });
 
-    describe("when its stock share price valorizes", function(){
-        beforeEach(function(){
+    describe("when its stock share price valorizes", function() {
+        beforeEach( function(){
             stock.sharePrice = 40;
+        });
+
+        it("should have a positive return of investment", function() {
+           expect( investment.roi() ).toEqual( 1 );
+        });
+
+        it("should be a good investment", function() {
+            expect( investment.isGood() ).toBeTruthy();
         });
     });
 
