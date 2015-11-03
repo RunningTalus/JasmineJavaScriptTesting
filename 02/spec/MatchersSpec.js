eb9681db42021bef2e4f483c0f92e81badb6f015
Dave May <dave@davemay.org>
2015-11-02 22:26:36 -0700

    Using Jasmine's toBe Built-in Matcher. Uses strict equals operator (===).  Use toBe matcher when you want to check if two variables reference the same Object.

--- 02/spec/MatchersSpec.js
+++ 02/spec/MatchersSpec.js
@@ -21,4 +21,36 @@ describe("Jasmine Built-in matchers", function() {
         });
     });
 
+    describe("toBe", function() {
+        it("should pass equal numbers", function() {
+            expect(1).toBe(1);
+        });
+
+        it("should pass equal strings", function() {
+            expect("testing").toBe("testing");
+        });
+
+        it("should pass equal booleans", function() {
+            expect(true).toBe(true);
+        });
+
+        it("should pass same objects", function() {
+            var object = {a: "testing"};
+            expect(object).toBe(object);
+        });
+
+        it("should pass same arrays", function() {
+            var array = [1, 2, 3];
+            expect(array).toBe(array);
+        });
+
+        it("should not pass equal objects", function() {
+            expect({a: "testing"}).not.toBe({a: "testing"});
+        });
+
+        it("should not pass equal arrays", function() {
+            expect([1, 2, 3]).not.toBe([1, 2, 3]);
+        });
+    });
+
 });
--- Notes.md
+++ Notes.md
@@ -1,2 +1,9 @@
 Built-in Matchers
+
 toEqual - check equality between two values. Works on primitive values (Boolean, Number, String) and Objects (including Arrays).
+
+toBe - Similar to toEqual, and provides the same result when comparing primitive values. 
+    Uses strict equals operator (===). 
+    NOTE: The strict equals operator always considers false any comparison between values of distinct types. 
+    Advised to use the toEqual operator in most cases, and use toBe matcher only when you want to check if two variables reference the same object type.
+
